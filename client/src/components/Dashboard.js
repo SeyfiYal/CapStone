1c4cc884721f5148221007f592356b26d25917c6
Seyfican Yalcindag <+>
2023-05-26 00:04:14 +0300

    ChatBot

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styling/DashBoard.css';
 
+// Create a JavaScript version of the ChatBot class
+class ChatBot {
+    intents = [
+        ['hi|hello|hey|hey there', ['Hey there!']],
+        ['what\'s up|sup|what\'s new', ['Not much, just chatting with you!', 'Just hanging out, how about you?', 'Nothing new, how about you?']],
+        ['how are you|how\'s it going|how have you been', ['I\'m good, how about you?', 'I\'ve been doing alright, thanks for asking! How about you?']],
+        ['.*', ['I\'m not sure what you mean, can you please rephrase that?', 'Sorry, I don\'t understand what you\'re asking!', 'I\'m not sure I know the answer to that!']],
+    ];
+
+    respond(userInput) {
+        for (let i = 0; i < this.intents.length; i++) {
+            let pattern = new RegExp(this.intents[i][0]);
+            if (pattern.test(userInput)) {
+                let responses = this.intents[i][1];
+                let response = responses[Math.floor(Math.random() * responses.length)];
+                return response;
+            }
+        }
+        return "Sorry, I didn't understand that.";
+    }
+}
+
 function Dashboard() {
-  return (
-    <div className="front-page">
-    <h1>Hello dashboard</h1>
-    </div>
+    const [messages, setMessages] = useState([]);
+    const [userInput, setUserInput] = useState("");
+    const chatbot = new ChatBot();
+
+    function sendMessage(e) {
+        e.preventDefault();  // prevent page refresh on form submit
+        if (userInput.toLowerCase() === 'quit') {
+            setMessages([
+                ...messages,
+                { text: 'THANKS FOR BEING HERE', sender: 'Over-Lord ' },
+            ]);
+        } else {
+            let botResponse = chatbot.respond(userInput);
+            setMessages([
+                ...messages,
+                { text: userInput, sender: 'Human ' },
+                { text: botResponse, sender: 'Over-Lord ' },
+            ]);
+        }
+        setUserInput("");  // clear the input field
+    }
+
+    function handleInputChange(e) {
+        setUserInput(e.target.value);
+    }
+
+    return (
+      <div className="front-page">
+          <h1>Hello dashboard</h1>
+          <div className="chat-box">
+              <div className="chat-area">
+                  {messages.map((message, index) => (
+                      <div key={index} className={`${message.sender}-msg`}>
+                          <p><strong>{message.sender}:</strong> {message.text}</p>
+                      </div>
+                  ))}
+              </div>
+              <form onSubmit={sendMessage}>
+                  <input type="text" value={userInput} onChange={handleInputChange} className="chat-input" />
+                  <button type="submit" className="chat-submit">Send</button>
+              </form>
+          </div>
+      </div>
   );
 }
 
 export default Dashboard;
-
